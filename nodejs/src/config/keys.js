02408736d5da13e41a83379e77be1ad25665a261
flukedev8 <sittisak.951753@gmail.com>
2020-03-16 06:17:21 +0700

    rm

--- nodejs/src/config/keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    database:{
-        host: 'localhost',
-        user: 'root',
-        password: 'HiKMIDS2020',
-        database: 'kmids_payment',
-        "JWT_KEY":'secret'
-    }
-};
